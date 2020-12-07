6f739d896f4c837b297805636fbfc943b79edc0b
Last <huan0016@gmail.com>
2020-12-08 01:43:07 +0800

    ws

---
+++ bin/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"flag"
+	"net/http"
+
+	"../../communication"
+)
+
+var addr = flag.String("addr", ":2333", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "../html/home.html")
+}
+
+func main() {
+	flag.Parse()
+	log.Println("http://localhost:2333")
+	hub := communication.NewHub()
+	go hub.Run()
+	http.HandleFunc("/", serveHome)
+	http.Handle("/ws", hub)
+	http.ListenAndServe(*addr, nil)
+}
---
+++ hub.go
@@ -0,0 +1,57 @@
+package communication
+
+import (
+	"log"
+	"net/http"
+)
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+func (hub *Hub) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+	go client.writePump()
+	go client.readPump()
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
